d2d72a7a78961ca526d229b004a3fca211859d54
Viuh <37941072+viuh@users.noreply.github.com>
2021-02-12 20:20:20 +0200

    +tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,44 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_varastooon_liikaa_kamaa(self):
+        self.varasto.lisaa_varastoon(20)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(),0)
+
+
+    def test_varastosta_liikaa_otettu(self):
+        saatu_maara = self.varasto.ota_varastosta(20)
+
+        self.assertAlmostEqual(saatu_maara,0)
+
+
+    def test_liianpieni_varasto(self):
+
+        Varasto2 = Varasto(10,-10)
+        self.assertAlmostEqual(Varasto2.paljonko_mahtuu(),10)
+
+    def test_liianpieni_tilavuus(self):
+
+        Varasto2=Varasto(-10,100)
+        self.assertAlmostEqual(Varasto2.paljonko_mahtuu(),10)
+
+
+    def test_ota_negatiivinen(self):
+        self.varasto.ota_varastosta(-2)
+
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(),10)
+
+    def test_lisaa_negatiivinen(self):
+
+        self.varasto.lisaa_varastoon(-5)
+        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(),10)
+
+
+    def test_tulostavarasto(self):
+
+        vastaus = str(self.varasto)
+        self.assertEqual(vastaus, "saldo = 0, vielä tilaa 10")
+
+
